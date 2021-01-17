ba454f5f05c7d385c20ae9aeaafe4f7848bdedf5
RuthlessNative <daniel.g.ramirez13@gmail.com>
2021-01-17 18:30:24 -0500

    Poll Results accurate. Prints to terminal, needs to write to file.

--- PyPoll/PyPoll_main.py
+++ PyPoll/PyPoll_main.py
@@ -9,16 +9,30 @@ with open(data_csv) as csv_file:
     header = next(csv_reader)
 
     tot_vote = 0
-    candiate_ls = []
+    candidate_ls = []
+    # Holds copy of the data to run through csv a second time
+    csv_reader2 = []
 
     for row in csv_reader:
+        # Assume each Voter ID is unique
         tot_vote += 1
-        if row[2] not in candiate_ls:
-            candiate_ls.append(row[2])
+        csv_reader2.append(row)
+        if row[2] not in candidate_ls:
+            candidate_ls.append(row[2])
 
+    # Create candidate dictionary (name:votes)
+    can_dict = {name: 0 for name in candidate_ls}
+
+    for row in csv_reader2:    
+        for k,v in can_dict.items():        
+            if row [2] == k:
+                can_dict[k] += 1
 
 print("Election Results")
 print ("-------------------------")
 print(f"Total Votes: {tot_vote}")
 print ("-------------------------")
-print(candiate_ls)
+
+for k,v in can_dict.items():
+    print (f"{k}: {round((v/tot_vote)*100,2)}% ({v})")
+
